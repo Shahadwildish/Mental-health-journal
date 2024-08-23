a7130f19a839d041f43398c9317d285d5ebfcc71
shahadwildish <shayalobeidi@gmail.com>
2024-08-23 14:41:50 -0400

    Fixed authContext  to grad userID from DB

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
 
         // Generate JWT token
         const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-        res.status(200).json({ token });
+        res.status(200).json({ token, userId: user.userId, email: user.email });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
